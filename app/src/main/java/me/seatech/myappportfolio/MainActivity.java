0f4d67ef88e0926aa3dd22bc5e52f4735a93eb3a
yesalam <itisalam@gmail.com>
2016-01-30 23:38:55 +0530

    The first project "Project 0" of Udacity Android Nanodegree.

---
+++ app/src/main/java/me/seatech/myappportfolio/MainActivity.java
@@ -0,0 +1,22 @@
+package me.seatech.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View v){
+        String string = (String) ((Button) v).getText();
+        Toast.makeText(this,"This button will launch my "+ string,Toast.LENGTH_LONG).show();
+    }
+}
